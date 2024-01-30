0e1e3cd6f8f5fb207bdc4192c6c59113b698d4e4
Emmanuel Uniana <78772230+EmmanuelUniana17@users.noreply.github.com>
2024-01-30 19:44:46 +0800

    Update tiltsensorwith1led_uniana1.ino

--- tiltsensorwith1led_uniana1.ino
+++ tiltsensorwith1led_uniana1.ino
@@ -1,6 +1,6 @@
 // 2024-01-30
 // Robotics: Tilt Sensor with 1 LED
-// Circuit ko: https://imgur.com/FsY9FGP
+// Circuit ko: https://imgur.com/uHhUH0y
 // Github Repository: https://github.com/EmmanuelUniana17/ArduinoProjects
 
 int led_yellow = 3; // Yellow LED
